d915178c34bfb175d97ff72ca96b918f56dc7ff6
Egor Shkirya <zuhijan@yandex.ru>
2020-03-17 18:59:10 +0600

    constans error and config

---
+++ src/js/constants/errors-text.js
@@ -0,0 +1,7 @@
+export const ERR_LOG_IN = "Неверный логин или пароль";
+export const ERR_LOG_UP = "Пользователь с такими данными уже существует";
+export const ERR_KEY_WORD = 'Нужно ввести ключевое слово';
+export const ERR_REQUIRE_FIELD = 'Это обязательное поле';
+export const ERR_MIN_MAX_LENGTH = 'Должно быть от 2 до 30 символов';
+export const ERR_EMAIL = 'Некорректный email';
+export const ERR_PASS = 'Пароль должен быть от 8 до 30 символов';
